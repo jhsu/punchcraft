1e85acbc9f4bd5166e974e88f455d539d81a023f
Joseph Hsu <jhsu@josephhsu.com>
2010-11-30 15:50:40 -0500

    implement movement of mouse and storing last position

--- mousecontrol.py
+++ mousecontrol.py
@@ -1,4 +1,5 @@
 from Xlib import X, display
+from numpy import matrix
 
 current_display = display.Display()
 current_screen = current_display.screen()
@@ -9,6 +10,7 @@ class MouseControl:
     def __init__(self, current_display=None):
         self.display = current_display.Display() if current_display else display.Display()
         self.mouse = self.mouse_position()
+        self.last_position = None
 
     def update(self):
         self.display.sync()
@@ -17,18 +19,26 @@ class MouseControl:
         data = self.display.screen().root.query_pointer()._data
         return data["root_x"], data["root_y"]
 
-    def offset_by(self, offset):
-        self.display.screen().root.warp_pointer(offset[0], offset[1])
+    def position(self):
+        if(self.last_position != None):
+            return self.last_position
+        else:
+            self.last_position = self.mouse_position()
+            return self.last_position
 
+    def to_target(self, target):
+        offset = matrix(target) - matrix(self.position())
+        self.display.warp_pointer(offset.flat[0], offset.flat[1])
+        self.last_position = target
+        self.sync()
+        return self
 
-def sync_display():
-    current_display.sync()
+    def mouse_to(self, position):
+        self.display.screen().root.warp_pointer(position[0], position[1])
+        self.sync()
 
-def mousepos():
-    """mousepos() --> (x, y) get the mouse coordinates on the screen (linux, Xlib)."""
-    data = current_root.query_pointer()._data
-    return data["root_x"], data["root_y"]
+    def reset(self):
+        self.last_position = None
 
-def mouse_move_to(offset):
-    current_root.warp_pointer(offset[0], offset[1])
-    sync_display()
+    def sync(self):
+        self.display.sync()
--- mousecontrol_test.py
+++ mousecontrol_test.py
@@ -6,8 +6,26 @@ class MouseControlTest(unittest.TestCase):
     def setUp(self):
         self.mc = MouseControl()
 
+
     def test_has_display(self):
         self.assertTrue(self.mc.display, 'MouseControl should be DISPLAY aware')
+
+    def test_can_get_mouse_position(self):
+        mouse_x, mouse_y = self.mc.mouse_position()
+        self.assertEqual(type(mouse_x), type(int()))
+
+    def test_move_mouse(self):
+        self.mc.mouse_to((5,5))
+        self.mc.last_position = initial_position = self.mc.mouse_position()
+        self.mc.to_target((10,10))
+        self.assertEqual((10,10), self.mc.position(), 'Mouse did not move to correct location')
+
+    def test_repeated_movement(self):
+        self.mc.mouse_to((5,5))
+        self.mc.reset()
+        self.mc.to_target((20,20))
+        self.assertEqual((20,20), self.mc.position())
+
 
 if __name__ == '__main__':
     unittest.main()
